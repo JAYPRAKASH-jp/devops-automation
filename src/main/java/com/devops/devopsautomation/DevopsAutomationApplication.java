f015b5afed1aa278128034f73ebbb33e62686050
Jayprakash Sah <jayprakashs@comsenseconsulting.com>
2022-12-27 11:09:56 +0530

    Added rest endpoint

--- src/main/java/com/devops/devopsautomation/DevopsAutomationApplication.java
+++ src/main/java/com/devops/devopsautomation/DevopsAutomationApplication.java
@@ -2,12 +2,20 @@ package com.devops.devopsautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsAutomationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsAutomationApplication.class, args);
+	}
+	
+	@GetMapping
+	public String msg() {
+		return "Hello Jay";
 	}
 
 }
